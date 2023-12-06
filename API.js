7dfddf65ea3498a4a7fd6819b6a6011881a00ce9
SUZANE ARCANJO DA HORA <23111828@CFP127ALUNO.LOCAL>
2023-12-06 17:21:54 -0300

    na metade

---
+++ API.js
@@ -0,0 +1,51 @@
+var tonypizzaria = {
+
+    usuario: [
+        {
+            id: '01',
+            nome: 'joao da Silva',
+            email: 'joaoSilvinha@gmail.com',
+            senha: 'joaoosilvaa123',
+            telefone: '647397748',
+            endereço: 'rua valdo',
+            foto: './img/joaoSilva.jfif',
+        },
+        {
+            id: '02',
+            nome: 'Maria Vitoria',
+            email: 'vihhtoria@gmail.com',
+            senha: 'mariariri444',
+            telefone: '9455695403',
+            endereço: 'rua emilio rocha',
+            foto: './img/mariaVitoria.jfif'
+        },
+        {
+            id: '03',
+            nome: 'Ana Luiza',
+            email: 'ribrieojulia@gmail.com',
+            senha: 'bts123321',
+            telefone: '4521145354',
+            endereço: 'rua santa maria',
+            foto: './img/anaLuiza.jfif'
+        },
+        {
+            id: '04',
+            nome: 'Lucas Pereira',
+            email: 'lucacaPEreiraaz@gmailcom',
+            senha: 'alnzokatop',
+            telefone: '4521366258',
+            endereço: 'rua lucia da graça',
+            foto: './img/lucasPereira.jfif'
+        },
+        {
+            id: '05',
+            nome: 'Cecilia Costa',
+            email: 'costa23cece@gmailcom',
+            senha: 'Senhadacece154',
+            telefone: '754998562',
+            endereço: 'rua calçadas das lembranças',
+            foto: './img/ceciliaCosta.jfif'
+        }
+    ]
+    
+}
